69d98c3ec7c76a10b6fde44ddd85f789055b7f6e
Pablo Marino <pmarino2013@gmail.com>
2020-08-01 16:07:29 -0300

    Renombrando build por doscs

---
+++ docs/precache-manifest.268d4fd8a1e1b028ed8cdb0814c88e20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48b62e16247bc693aca4cc550d713ed6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7804657e40facab388b9",
+    "url": "/static/css/main.bfb88dcf.chunk.css"
+  },
+  {
+    "revision": "8c79211cebf75eca7c79",
+    "url": "/static/js/2.74471001.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.74471001.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7804657e40facab388b9",
+    "url": "/static/js/main.92ae31eb.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
